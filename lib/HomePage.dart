e2b18adbd3f4a25b2dbfb100a41c4414576b845e
JESSICA\Jessica Polli <jessye_smo@hotmail.com>
2020-04-13 20:25:28 -0300

    Comit inicial. Tela de login e menus.

---
+++ lib/HomePage.dart
@@ -0,0 +1,306 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    Inicio(),
+    Recentes(),
+    Favoritos(),
+    Receitas(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Mestre Cuca App')
+      ),
+      drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+
+                accountName: Text('Nome Usuário'),
+                accountEmail: Text('email_usuario@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: AssetImage('assets/images/usuario.jpg'),
+                ),
+              ),
+              ListTile(
+                  title: Text('Item de menu 1'),
+                  leading: Icon(Icons.stars),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    debugPrint('Clicou no menu 1');
+                  }
+              ),
+              ListTile(
+                  title: Text('Configurações'),
+                  leading: Icon(Icons.settings),
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () {
+                    debugPrint('Clicou no menu 2');
+                  }
+               ),
+              ListTile(
+                  title: Text('Item de menu 2'),
+                  leading: Icon(Icons.account_circle),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    debugPrint('Clicou no menu 2');
+                  }
+              ),
+            ],
+          )
+      ),
+      body:_widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) => setState(() {
+            setState(() {
+              _selectedIndex = index;
+            });
+          }),
+          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          currentIndex: _selectedIndex,
+          items:const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home,),
+                title: Text('Início')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_time),
+                title: Text('Recentes'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border,),
+                title: Text('Favoritas', )
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.restaurant),
+              title: Text('Receitas'),
+            ),
+          ]
+      ),
+    );
+  }
+}
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(top: 5),
+      child: Column(
+
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/fritas.jpg",
+                    ),
+                  )
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                margin: EdgeInsets.only(left: 6, right: 3),
+                height: MediaQuery.of(context).size.height*.36,
+                width: MediaQuery.of(context).size.width*.47,
+                child:InkWell(
+                  child:  Image.asset(
+                    "assets/images/sobremesa.jpg",
+                  ),
+                )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/assados.jpg",
+                    ),
+                  )
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.25,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/bolo.jpg",
+                    ),
+                  )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/cafe.jpg",
+                    ),
+                  )
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/cupcake.jpg",
+                    ),
+                  )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6,right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/donuts.jpg",
+                    ),
+                  )
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/drinks.jpg",
+                    ),
+                  )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/fitness.jpg",
+                    ),
+                  )
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  margin: EdgeInsets.only(left: 6, right: 3),
+                  height: MediaQuery.of(context).size.height*.36,
+                  width: MediaQuery.of(context).size.width*.47,
+                  child:InkWell(
+                    child:  Image.asset(
+                      "assets/images/sorvete.jpg",
+                    ),
+                  )
+              ),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+class Recentes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Recentes")
+    );
+  }
+}
+
+class Favoritos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Favoritos"),
+    );
+  }
+}
+
+
+class Receitas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Minhas Receitas"),
+    );
+  }
+}
---
+++ lib/LoginPage.dart
@@ -0,0 +1,142 @@
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextStyle style = TextStyle(fontSize: 20, color: Colors.white);
+  String _usuario="";
+  String _senha="";
+  final frmLoginKey = GlobalKey<FormState>();
+
+  void _validarLogin(){
+    final form =frmLoginKey.currentState;
+    if(form.validate()){
+      form.save();
+      if(_usuario=="teste12" && _senha=="teste12" ){
+        Navigator.of(context).pushNamedAndRemoveUntil( "/HomePage", (route) => false);
+      }else{
+        showDialog(
+            context: context,
+            builder: (context){
+              return AlertDialog(
+                title: Text("Erro Login"),
+                content: Text("Usuário e/ou senha inválidos!"),
+              );
+            }
+        );
+      }
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    final usuarioField= TextFormField(
+      obscureText: false,
+      style: style,
+      onSaved: (valor){
+        _usuario = valor;
+      },
+      validator: (valor){
+        return valor.length<6? "Usuário deve ter no mínimo 6 caracteres": null;
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        labelText:  'Usuário',
+        labelStyle: TextStyle(fontSize: 16),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32), ),
+        enabledBorder:  OutlineInputBorder(borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(32), ),
+      ),
+    );
+    final senhaField= TextFormField(
+      obscureText: true,
+      style: style,
+      onSaved: (valor){
+        _senha = valor;
+      },
+      validator: (valor){
+        return valor.length<6? "Senha deve ter no minimo 6 caracteres": null;
+      },
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2.0, style: BorderStyle.solid), borderRadius: BorderRadius.circular(32), ),
+        enabledBorder:  OutlineInputBorder(borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(32), ),
+        labelText:  'Senha',
+        labelStyle: TextStyle(fontSize: 16)
+      ),
+    );
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                color: Colors.black,
+                child: Padding(
+                    padding: const EdgeInsets.all(36),
+                    child: Form(
+                      key: frmLoginKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 155,
+                            child: Image.asset('assets/images/logo.png'),
+                          ),
+                          SizedBox(
+                            height: 45,
+                          ),
+                          usuarioField,
+                          SizedBox(
+                            height: 15,
+                          ),
+                          senhaField,
+                          SizedBox(
+                            height: 25,
+                          ),
+                          RaisedButton(
+                            padding: EdgeInsets.all(20),
+                            color: Colors.lightBlue,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width*.85,
+                              child: Text('LOGIN', style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
+                            ),
+                            focusColor: Color(0xFFCF9F77),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                                side: BorderSide(color: Colors.blue)
+                            ),
+                            elevation: 7.0,
+                            onPressed: (){
+                              _validarLogin();
+                            },
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          FlatButton(
+                              onPressed: () => showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                        content: Text('Acesso a tela de registro de usuários!')
+                                    );
+                                  }
+                              ),
+                              child: Text('Registre-se',
+                                  style: TextStyle(fontSize: 25, color:Color(0xFFffffff),)
+                              )
+                          )
+                        ],
+                      ),
+                    )
+                ),
+              )
+          ),
+        )
+    );
+  }
+}
